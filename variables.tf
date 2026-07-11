657b49f1687c7cf4b5f680883d840228d8249aca
GersonRS <gersonrodriguessantos8@gmail.com>
2026-07-11 18:34:08 -0300

    feat: add namespace_labels variable to all modules

--- variables.tf
+++ variables.tf
@@ -87,6 +87,14 @@ variable "dependency_ids" {
   default     = {}
 }
 
+variable "namespace_labels" {
+  description = "Labels to apply to the destination namespace managed by Argo CD (requires CreateNamespace=true)."
+  type        = map(string)
+  default = {
+    "istio.io/dataplane-mode" = "ambient"
+  }
+}
+
 #######################
 ## Module variables
 #######################
